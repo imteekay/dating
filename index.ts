211ec1c5356a71fedca56ab51b4dc4ba06eda47f
TK <leandrotk100@gmail.com>
2020-04-02 21:18:03 -0300

    Days ago function

--- index.ts
+++ index.ts
@@ -1,23 +1,49 @@
-const beginningOfDay = (): Date => {
+type SeparatedDate = {
+  day: number
+  month: number
+  year: number
+};
+
+type DaysAgo = {
+  daysAgo: Date
+};
+
+const getSeparatedDate = (): SeparatedDate => {
   const now: Date = new Date();
   const day: number = now.getDate();
   const month: number = now.getMonth();
   const year: number = now.getFullYear();
 
+  return {
+    day,
+    month,
+    year
+  };
+};
+
+const beginningOfDay = (): Date => {
+  const { day, month, year }: SeparatedDate = getSeparatedDate();
+
   return new Date(year, month, day);
 };
 
 const beginningOfMonth = (): Date => {
-  const now: Date = new Date();
-  const month: number = now.getMonth();
-  const year: number = now.getFullYear();
+  const { month, year }: SeparatedDate = getSeparatedDate();
 
   return new Date(year, month, 1);
 };
 
 const beginningOfYear = (): Date => {
-  const now: Date = new Date();
-  const year: number = now.getFullYear();
+  const { year }: SeparatedDate = getSeparatedDate();
 
   return new Date(year, 0, 1);
+};
+
+const get = (n: number): DaysAgo => {
+  const { day, month, year }: SeparatedDate = getSeparatedDate();
+  const daysAgo: Date = new Date(year, month, day - n);
+
+  return {
+    daysAgo
+  };
 };
